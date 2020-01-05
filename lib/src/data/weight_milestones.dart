b8a20a043bf8a1a13dc526b9ff3a7a828999a2a9
ThatGreyDragon <ThatGreyDragon@users.noreply.github.com>
2020-01-04 19:23:39 -0500

    Fix formatting in popups

--- lib/src/data/weight_milestones.dart
+++ lib/src/data/weight_milestones.dart
@@ -2,7 +2,14 @@ import 'package:go_hoard_yourself/src/models/popup.dart';
 
 final Map<double, Popup> WEIGHT_MILESTONES = {
   150: Popup('Working hard, or...?', '''
-    Uh oh! Looks like you\'re getting a bit big.
-    Your work rate will slow down the bigger you get, so watch out!
+    You're beginning to feel the effects of your diet.
+    The last time you went hunting, you were so out of breath
+    by the end of it you caught half of what you normally do.
+    It looks like from here on out, kobolds are going to have to
+    pick up some of the slack...
+
+    (As your weight increases, the speed at which you manually
+    work on tasks will slow down. Ensure you have kobolds assigned
+    to the tasks you need to get done.)
   '''),
 };
